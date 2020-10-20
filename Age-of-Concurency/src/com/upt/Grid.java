4a52d0cb957c336f2852c59a144e02f3003c0c3c
ewald <ewald.dobre@gmail.com>
2020-10-20 17:36:37 +0300

    implemented scenarios;

--- Age-of-Concurency/src/com/upt/Grid.java
+++ Age-of-Concurency/src/com/upt/Grid.java
@@ -1,6 +1,9 @@
 package com.upt;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Grid
 {
@@ -17,6 +20,42 @@ public class Grid
     }
 
     private int height;
+
+
+    public Grid(String pathname) {
+
+        ArrayList<String> lines = new ArrayList<String>();
+        Scanner input = null;
+
+        try {
+            input = new Scanner(new File(pathname));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        while(input.hasNextLine())
+        {
+            Scanner colReader = new Scanner(input.nextLine());
+            while(colReader.hasNext())
+            {
+                lines.add(colReader.nextLine());
+            }
+        }
+
+        this.height = lines.size();
+        this.width = lines.get(0).length();
+        tiles = new Tile[width][height];
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+//                System.out.print(lines.get(y).charAt(x));
+                tiles[x][y] = new Tile(x, y, lines.get(y).charAt(x));
+            }
+            System.out.println();
+        }
+    }
+
+
 
     public Grid(int width, int height)
     {
@@ -53,9 +92,9 @@ public class Grid
     }
 
     public void displayGrid() {
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                System.out.print(tiles[i][j].getSymbol());
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                System.out.print(tiles[x][y].getSymbol());
             }
             System.out.println();
         }
--- Age-of-Concurency/src/com/upt/Main.java
+++ Age-of-Concurency/src/com/upt/Main.java
@@ -1,11 +1,16 @@
 package com.upt;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        Grid grid = new Grid(4, 4);
-        grid.displayGrid();
+    public static void main(String[] args) throws FileNotFoundException {
 
+        Grid grid = new Grid("Age-of-Concurency\\scenarios\\1.txt");
+        grid.displayGrid();
 
     }
 }
